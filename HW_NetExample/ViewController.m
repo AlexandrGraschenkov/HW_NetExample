1ccfec4734067e2601d86a985d4d36dcaa2f4b49
leonspok <igor9774@gmail.com>
2014-04-29 13:14:29 +0400

    Task completed

--- HW_NetExample/ViewController.m
+++ HW_NetExample/ViewController.m
@@ -18,18 +18,18 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // тут должно быть что-то
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    PresentImageController *controller = [segue destinationViewController];
+    
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    [controller setImageInfo:[presentData objectAtIndex:indexPath.row]];
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
     [self reloadData];
@@ -37,26 +37,40 @@
 
 - (void)reloadData
 {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+    [view setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.7]];
+    [view.layer setCornerRadius:5.0f];
+    view.center = CGPointMake(self.view.bounds.size.width / 2.0, self.view.bounds.size.height / 2.0);
+    [self.view addSubview:view];
+    
     UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
     indicator.tintColor = [UIColor blackColor];
-    indicator.center = CGPointMake(self.view.bounds.size.width / 2.0, self.view.bounds.size.height / 2.0);
+    indicator.center = CGPointMake(view.bounds.size.width / 2.0, view.bounds.size.height / 2.0);
     indicator.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin;
-    [self.view addSubview:indicator];
+    [view addSubview:indicator];
     [indicator startAnimating];
     
     
     [[MyNetManager sharedInstance] getAsyncImagesInfo:^(NSArray *imagesInfo) {
-        [indicator removeFromSuperview];
-        
-        // обновление данных
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [view removeFromSuperview];
+            presentData = imagesInfo;
+            [self.tableView reloadData];
+        });
     }];
 }
 
 #pragma mark - Table
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 0;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return presentData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    [cell.textLabel setText:[[presentData objectAtIndex:indexPath.row] objectForKey:@"folder_name"]];
+    return cell;
 }
 
 @end
