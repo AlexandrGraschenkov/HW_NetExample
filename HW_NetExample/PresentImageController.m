ef4a09de5cd528a4a176bc02d0ea6e7c1d2ecc6a
Olga <Olga@Mac-mini-administrator-43.local>
2014-06-20 17:12:24 +0400

    Done

--- HW_NetExample/PresentImageController.m
+++ HW_NetExample/PresentImageController.m
@@ -14,6 +14,7 @@
 @property (nonatomic, weak) IBOutlet UIScrollView *scroll;
 @end
 
+
 @implementation PresentImageController
 
 - (void)viewDidLoad
@@ -28,6 +29,7 @@
     [super viewWillAppear:animated];
     
     [self reloadAndPresentData];
+    
 }
 
 - (void)reloadAndPresentData
@@ -35,9 +37,36 @@
     for(UIView *v in self.scroll.subviews){
         [v removeFromSuperview];
     }
+    NSString *folderName =[self.imageInfo objectForKey:@"folder_name"];
+   // NSLog(@"Folder Name%@",folderName);
+    int row = [[self.imageInfo objectForKey:@"columns_count"]intValue] ;
+    int column = [[self.imageInfo objectForKey:@"rows_count"]intValue] ;
+    int width = [[self.imageInfo objectForKey:@"elem_width"]intValue] ;
+    int height = [[self.imageInfo objectForKey:@"elem_height"]intValue];
+    self.scroll.contentSize = CGSizeMake(column * width,  row * height);
+    //NSLog(@"PRESENIMAGE");
+  
+    for (int i= 0; i<row ;i++){
+        for(int j = 0; j<column; j++){
     
-//    NSURL *imgURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://dl.dropboxusercontent.com/u/55523423/NetExample/%@/%d_%d.png", folderName, col, row]];
-    
+            NSURL *imgURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://dl.dropboxusercontent.com/u/55523423/NetExample/%@/%d_%d.png", folderName ,i , j]];
+            [[MyNetManager sharedInstance]getAsyncImageWithURL:imgURL complection:^(UIImage *img){
+                dispatch_async(dispatch_get_main_queue(), ^(){
+                   
+                   UIImageView *imgView = [[UIImageView alloc]initWithFrame:CGRectMake(j*width,i*height,width,height ) ];
+                    
+                    imgView.image = img;
+                    [self.scroll addSubview:imgView];
+                    [self.view setNeedsDisplay];
+                    [self.scroll setNeedsDisplay];
+                    
+                });
+            }];
+           
+            
+            
+    }
+    }
 }
 
 @end
--- HW_NetExample/ViewController.m
+++ HW_NetExample/ViewController.m
@@ -21,42 +21,62 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [[MyNetManager sharedInstance] getAsyncImagesInfo:^(NSArray *imagesInfo) {
+      
+       // NSLog(@"images loaded");
+       // NSLog(@"%@", imagesInfo);
+        presentData = imagesInfo;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[self tableView] reloadData];
+        });
+        
+    }];
+    
+
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // тут должно быть что-то
+   
+    PresentImageController *imgController = [segue destinationViewController];
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    imgController.imageInfo = [presentData objectAtIndex:indexPath.row];
+   // NSLog(@"PRESENT DATA %@", [presentData objectAtIndex:indexPath.row]);
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
     [self reloadData];
 }
 
 - (void)reloadData
 {
+   UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+   [self.view addSubview:view];
     UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
     indicator.tintColor = [UIColor blackColor];
     indicator.center = CGPointMake(self.view.bounds.size.width / 2.0, self.view.bounds.size.height / 2.0);
     indicator.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin;
     [self.view addSubview:indicator];
+    
     [indicator startAnimating];
     
-    
-    [[MyNetManager sharedInstance] getAsyncImagesInfo:^(NSArray *imagesInfo) {
-        [indicator removeFromSuperview];
-        
-        // обновление данных
-    }];
-}
+   }
 
 #pragma mark - Table
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return [presentData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    cell.textLabel.text = [[presentData objectAtIndex:indexPath.row]objectForKey:@"folder_name"];
+    return cell;
 }
 
 @end
